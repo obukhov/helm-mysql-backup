234eb4ac229e38124083a1d5a8be9b00348c2f89
Alex Obukhov <a.a.obukhov@gmail.com>
2020-03-10 23:57:54 +0100

    Fix app version

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+helm package --destination packages mysql-backup
+cr upload \
+    --owner obukhov \
+    --git-repo helm-mysql-backup \
+    --package-path packages \
+    --token $GITHUB_TOKEN
+
+cr index \
+    --owner obukhov \
+    --git-repo helm-mysql-backup \
+    --package-path packages \
+    --charts-repo https://obukhov.github.io/helm-mysql-backup/ \
+    --index-path .
